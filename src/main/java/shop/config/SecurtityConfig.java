ed555b75c551faec398ba772b81b8249345948c6
Mr Cai <ct948956918@163.com>
2018-07-16 14:33:05 +0800

    shop前端项目ssm整合

---
+++ src/main/java/shop/config/SecurtityConfig.java
@@ -0,0 +1,14 @@
+package shop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity //获取组件springsecurityFilterchain
+@EnableGlobalMethodSecurity(prePostEnabled=true)//开启方法级别的权限检验
+public class SecurtityConfig extends WebSecurityConfigurerAdapter{
+	
+	
+}
